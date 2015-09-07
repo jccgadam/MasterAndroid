6dd07db11b1311fd84bb7911cd6e84928f7eb139
jccgadam <jccgadam@hotmail.com>
2015-09-07 16:03:36 -0700

    First commit

---
+++ src/com/learn2crack/library/JSONParser.java
@@ -0,0 +1,155 @@
+package com.learn2crack.library;
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class JSONParser {
+
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = "";
+
+    // constructor
+    public JSONParser() {
+
+    }
+
+    public JSONObject getJSONFromUrl(String url, List<NameValuePair> params) {
+
+        // Making HTTP request
+        try {
+            // defaultHttpClient
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+//          System.out.println(params);
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params);
+//            httpPost.setHeader("Content-Type", "application/json");
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+
+            is = httpEntity.getContent();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            System.out.println("string buffer"+sb);
+            is.close();
+            json = sb.toString();
+            Log.e("JSON", json);
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+        	Log.e("response string",json);
+            jObj = new JSONObject(json);
+            
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+
+    }
+
+
+    public JSONObject getJSONFromUrl(String url, JSONObject params) {
+
+        // Making HTTP request
+        try {
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            HttpPost httpPostRequest = new HttpPost(url);
+            // defaultHttpClient
+//            DefaultHttpClient httpClient = new DefaultHttpClient();
+//            HttpPost httpPost = new HttpPost(url);
+////          System.out.println(params);
+//            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params);
+////            httpPost.setHeader("Content-Type", "application/json");
+//            HttpResponse httpResponse = httpClient.execute(httpPost);
+//            HttpEntity httpEntity = httpResponse.getEntity();
+//
+//            is = httpEntity.getContent();
+            StringEntity se;
+            se = new StringEntity(params.toString());
+
+            // Set HTTP parameters
+            httpPostRequest.setEntity(se);
+            httpPostRequest.setHeader("Accept", "application/json");
+            httpPostRequest.setHeader("Content-type", "application/json");
+            httpPostRequest.setHeader("Accept-Encoding", "gzip");
+            HttpResponse response = (HttpResponse) httpclient.execute(httpPostRequest);
+            is = response.getEntity().getContent();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            System.out.println("string buffer"+sb);
+            is.close();
+            json = sb.toString();
+            Log.e("JSON", json);
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+            Log.e("response string",json);
+            jObj = new JSONObject(json);
+
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+
+    }
+}
---
+++ src/com/learn2crack/library/UserFunctions.java
@@ -0,0 +1,192 @@
+package com.learn2crack.library;
+
+/**
+ * Author :Raj Amal
+ * Email  :raj.amalw@learn2crack.com
+ * Website:www.learn2crack.com
+ **/
+
+import java.util.*;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.content.Context;
+
+import javax.net.ssl.SSLContext;
+
+
+public class UserFunctions {
+
+    private JSONParser jsonParser;
+    
+  /*//URL of the PHP API WORKING LOCAL ASUS WAMPSERVER
+    private static String loginURL = "http://192.168.4.110/learn2crack_login_api/";
+    private static String registerURL = "http://192.168.4.110/learn2crack_login_api/";
+    private static String forpassURL = "http://192.168.4.110/learn2crack_login_api/";
+    private static String chgpassURL = "http://192.168.4.110/learn2crack_login_api/";*/
+    
+  /*//URL of the PHP API WORKING PERSONAL PC
+    private static String loginURL = "http://192.168.0.18:82/learn2crack_login_api/";
+    private static String registerURL = "http://192.168.0.18:82/learn2crack_login_api/";
+    private static String forpassURL = "http://192.168.0.18:82/learn2crack_login_api/";
+    private static String chgpassURL = "http://192.168.0.18:82/learn2crack_login_api/";*/
+
+    
+    //URL of the PHP API WORKING!!!!!!
+    private static String loginURL = "http://10.0.2.2:9000/signin";
+    private static String registerURL = "http://10.0.2.2:9000/signup";
+    private static String forpassURL = "http://unps.comli.com/learn2crack_login_api/index.php";
+    private static String chgpassURL = "http://unps.comli.com/learn2crack_login_api/index.php";
+    
+    /*//URL of the PHP API WORKING!!!!!
+    private static String loginURL = "http://api.learn2crack.com/android/loginapi/";
+    private static String registerURL = "http://api.learn2crack.com/android/loginapi/";
+    private static String forpassURL = "http://api.learn2crack.com/android/loginapi/";
+    private static String chgpassURL = "http://api.learn2crack.com/android/loginapi/";*/
+
+
+    private static String login_tag = "login";
+    private static String register_tag = "register";
+    private static String forpass_tag = "forpass";
+    private static String chgpass_tag = "chgpass";
+
+
+    // constructor
+    public UserFunctions(){
+        jsonParser = new JSONParser();
+    }
+
+    private static JSONObject getJsonObjectFromMap(Map params) throws JSONException {
+
+        //all the passed parameters from the post request
+        //iterator used to loop through all the parameters
+        //passed in the post request
+        Iterator iter = params.entrySet().iterator();
+
+        //Stores JSON
+        JSONObject holder = new JSONObject();
+
+        //using the earlier example your first entry would get email
+        //and the inner while would get the value which would be 'foo@bar.com'
+        //{ fan: { email : 'foo@bar.com' } }
+
+        //While there is another entry
+        while (iter.hasNext())
+        {
+            //gets an entry in the params
+            Map.Entry pairs = (Map.Entry)iter.next();
+
+            //creates a key for Map
+            String key = (String)pairs.getKey();
+
+            //Create a new map
+            Map m = (Map)pairs.getValue();
+
+            //object for storing Json
+            JSONObject data = new JSONObject();
+
+            //gets the value
+            Iterator iter2 = m.entrySet().iterator();
+            while (iter2.hasNext())
+            {
+                Map.Entry pairs2 = (Map.Entry)iter2.next();
+                data.put((String)pairs2.getKey(), (String)pairs2.getValue());
+            }
+
+            //puts email and 'foo@bar.com'  together in map
+            holder.put(key, data);
+        }
+        return holder;
+    }
+    /**
+     * Function to Login
+     **/
+
+    public JSONObject loginUser(String email, String password) throws JSONException {
+        // Building Parameters
+        JSONObject jsonObjSend = new JSONObject();
+        jsonObjSend.put("email",email.toString());
+        jsonObjSend.put("password",password.toString());
+
+        JSONObject json=jsonParser.getJSONFromUrl(loginURL, jsonObjSend);
+
+        //System.out.println("from userFunction.login"+json);
+        return json;
+    }
+
+    /**
+     * Function to change password
+     **/
+
+    public JSONObject chgPass(String newpas, String email){
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("tag", chgpass_tag));
+
+        params.add(new BasicNameValuePair("newpas", newpas));
+        params.add(new BasicNameValuePair("email", email));
+        JSONObject json = jsonParser.getJSONFromUrl(chgpassURL,params);
+
+        return json;
+    }
+
+
+
+
+
+    /**
+     * Function to reset the password
+     **/
+
+    public JSONObject forPass(String forgotpassword){
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("tag", forpass_tag));
+        params.add(new BasicNameValuePair("forgotpassword", forgotpassword));
+        JSONObject json = jsonParser.getJSONFromUrl(forpassURL,params);
+        return json;
+    }
+
+
+
+
+
+
+     /**
+      * Function to  Register
+      **/
+    public JSONObject registerUser(String fname, String lname, String email, String uname, String password){
+        // Building Parameters
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("tag", register_tag));
+        params.add(new BasicNameValuePair("fname", fname));
+        params.add(new BasicNameValuePair("lname", lname));
+        params.add(new BasicNameValuePair("email", email));
+        params.add(new BasicNameValuePair("uname", uname));
+        params.add(new BasicNameValuePair("password", password));
+        JSONObject json = jsonParser.getJSONFromUrl(registerURL,params);
+        return json;
+    }
+
+    public JSONObject registerUser(String email, String password) throws JSONException {
+        // Building Parameters
+        JSONObject jsonObjectSend  = new JSONObject();
+        jsonObjectSend.put("email",password.toString());
+        jsonObjectSend.put("password",password.toString());
+        JSONObject json = jsonParser.getJSONFromUrl(registerURL,jsonObjectSend);
+        return json;
+    }
+
+
+    /**
+     * Function to logout user
+     * Resets the temporary data stored in SQLite Database
+     * */
+    public boolean logoutUser(Context context){
+        DatabaseHandler db = new DatabaseHandler(context);
+        db.resetTables();
+        return true;
+    }
+
+}
+
